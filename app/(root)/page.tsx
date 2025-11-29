2fce50c764ac5881cf43205028f8a2248e30c096
Kyrylo Prylipko <24914627+reend@users.noreply.github.com>
2025-11-29 13:45:53 +0100

    mock data for home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,15 @@
 import BookOverview from "@/components/BookOverview";
 import BookList from "@/components/BookList";
+import { sampleBooks } from "@/constants";
 
 const Home = () => (
   <>
-    <BookOverview />
-    <BookList />
+    <BookOverview {...sampleBooks[0]} />
+    <BookList
+      title="Latest Books"
+      books={sampleBooks}
+      containerClassName="mt-28"
+    />
   </>
 );
 
